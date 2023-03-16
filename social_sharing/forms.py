4927c9bf6eea5cdf48b77ce431a303ba1774eff8
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2023-03-17 07:29:28 +0800

    .save-button-container finish

--- social_sharing/forms.py
+++ social_sharing/forms.py
@@ -1,8 +1,12 @@
-from .models import Pin
+from .models import Pin, Board
 from django import forms
 
 class PinForm(forms.ModelForm):
     class Meta:
         model = Pin
-        exclude = ['board']
-        field = '__all__'
+        fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super(PinForm, self).__init__(*args, **kwargs)
+        self.fields['board'].empty_label = None
+        self.fields['board'].initial = Board.objects.first()        
--- social_sharing/models.py
+++ social_sharing/models.py
@@ -6,15 +6,24 @@ class Board(models.Model):
     is_secret = models.BooleanField(default=False)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Pin(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     destination_link = models.URLField(max_length=200)
     image = models.ImageField(upload_to='pins')
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     text = models.TextField()
     hearts = models.IntegerField(default=0)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.text[:30]
